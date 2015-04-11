f047532548a8ca3bdd4596fc700be26308bfdf55
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-11 13:26:34 -0400

    hello world runner

---
+++ lib/hello_world.ex
@@ -0,0 +1,12 @@
+defmodule ElixirKoans do
+  def hello_world do
+    IO.puts "hello world"
+    IO.puts "welcome to the Elixir koans"
+    IO.puts "let us start with the simplest failure."
+    IO.puts "Your Journey begins in test/hello_world.exs"
+    IO.puts "run `mix test test/hello_world.exs` to begin"
+  end
+end
+
+ElixirKoans.hello_world
+
